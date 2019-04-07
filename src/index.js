b264ea20ae862a82ed6e6e79a81ad1bc63ff351b
Lance Purnell <lance.purnell@hotmail.fr>
2019-04-07 19:53:50 +0800

    refactoring a class to a function

--- src/index.js
+++ src/index.js
@@ -1,41 +1,62 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
-// had to run 'npm install semantic-ui-css' and import the below for the icons to show from SeasonDisplay.js
-import 'semantic-ui-css/semantic.min.css';
 
-class App extends React.Component {
-  state = {lat: null, errorMessage: ''};
-  // above line is the refactored version of the below constructor method
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {lat: null, errorMessage: ''};
-  // };
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
 
-
-  componentDidMount() {
+  useEffect(() => {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({lat: position.coords.latitude}),
-      (err) => this.setState({errorMessage: err.message})
-    )
-  };
+      (position) => setLat(position.coords.latitude),
+      (err) => setErrorMessage(err.message)
+    );
+  }, []);
 
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    } else if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}/>;
-    } else if (!this.state.errorMessage && !this.state.lat) {
-      return <Spinner message="Please accept location request"/>;
-    };
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request" />;
   }
 
-  render() {
-    return (
-      <div className="border red">{this.renderContent()}</div>
-    )
-  };
-}
+  return <div className="border red">{content}</div>
+};
+
+// class App extends React.Component {
+//   state = {lat: null, errorMessage: ''};
+//   // above line is the refactored version of the below constructor method
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {lat: null, errorMessage: ''};
+//   // };
+
+
+//   componentDidMount() {
+//     window.navigator.geolocation.getCurrentPosition(
+//       (position) => this.setState({lat: position.coords.latitude}),
+//       (err) => this.setState({errorMessage: err.message})
+//     )
+//   };
+
+//   renderContent() {
+//     if (this.state.errorMessage && !this.state.lat) {
+//       return <div>Error: {this.state.errorMessage}</div>;
+//     } else if (!this.state.errorMessage && this.state.lat) {
+//       return <SeasonDisplay lat={this.state.lat}/>;
+//     } else if (!this.state.errorMessage && !this.state.lat) {
+//       return <Spinner message="Please accept location request"/>;
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div className="border red">{this.renderContent()}</div>
+//     )
+//   };
+// }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
